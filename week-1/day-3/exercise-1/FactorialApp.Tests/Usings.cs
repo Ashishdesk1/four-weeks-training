1d87b10711fc170313a0e30e8bcc579f32c65a74
mayurrkulkarni15 <mayurrkulkarni01@gmail.com>
2023-05-10 17:55:39 +0530

    Shortpath

--- week-1/day-3/exercise-1/FactorialApp.Tests/Usings.cs
+++ week-1/day-3/exercise-1/FactorialApp.Tests/Usings.cs
@@ -1,1 +1,22 @@
-global using NUnit.Framework;
+global using NUnit.
+    namespace FactorialApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            long factorial = CalculateFactorial(number);
+
+            Console.WriteLine($"The factorial of {number} is: {factorial}");
+        }
+
+        public static long CalculateFactorial(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+Framework;
